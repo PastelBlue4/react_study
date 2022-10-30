8514d6156f6e0d510bc918102a58c0ef8b45a148
Romuru <pastelblue0721@gmail.com>
2022-10-30 23:54:49 +0900

    [refactor] divide post form

--- src/week3/components/PostList.js
+++ src/week3/components/PostList.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
 import Post from "./Post";
+import WriteForm from "./WriteForm";
 
 const Container = styled.div`
   display: flex;
@@ -40,57 +41,8 @@ const ButtonContainer = styled.div`
   width: 50%;
 `;
 
-const WirteContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const FormContainer = styled.form`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const WriteTitle = styled.h1`
-  font-size: 32px;
-  color: beige;
-  margin-bottom: 20px;
-`;
-
-const StyledPostTitleInput = styled.input`
-  width: 600px;
-  height: 30px;
-  font-size: 16px;
-`;
-
-const StyledPostContentsTextA = styled.textarea`
-  width: 600px;
-  height: 300px;
-  margin-top: 50px;
-  font-size: 16px;
-`;
-
-const PostSubmutButton = styled.button`
-  margin-top: 15px;
-  padding: 15px 25px;
-  border-radius: 15px;
-  color: AAC4FF;
-  font-size: 16px;
-`;
-
 function PostList({ userName }) {
   const [isWrite, setIsWrite] = useState(false);
-  const [title, setTitle] = useState("");
-  const [contents, setContents] = useState("");
-  const dateObject = new Date();
-  console.log(dateObject);
-  const year = dateObject.getFullYear();
-  const month = dateObject.getUTCMonth() + 1;
-  console.log(month);
-  const day = dateObject.getDate();
 
   const [postItems, setpostItems] = useState([
     {
@@ -124,39 +76,11 @@ function PostList({ userName }) {
     setIsWrite(true);
   };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    let object = {
-      title: title,
-      date: `${year}년 ${month}월 ${day}일`,
-      contents: contents,
-    };
-    setpostItems((oldArray) => [object, ...oldArray]);
-    setIsWrite(false);
-  };
   return (
     <>
       {isWrite ? (
         <>
-          <WirteContainer>
-            <WriteTitle>입력폼</WriteTitle>
-            <FormContainer>
-              <StyledPostTitleInput
-                onChange={(e) => {
-                  setTitle(e.target.value);
-                }}
-                placeholder="제목을 입력해 주세용."
-              />
-              <StyledPostContentsTextA
-                onChange={(e) => {
-                  setContents(e.target.value);
-                }}
-                placeholder="내용을 입력해 주세용."
-              />
-              <PostSubmutButton onClick={onSubmit}>작성완료</PostSubmutButton>
-            </FormContainer>
-          </WirteContainer>
+          <WriteForm />
         </>
       ) : (
         <Container>
---
+++ src/week3/components/WriteForm.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+
+const WirteContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const FormContainer = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const WriteTitle = styled.h1`
+  font-size: 32px;
+  color: beige;
+  margin-bottom: 20px;
+`;
+
+const StyledPostTitleInput = styled.input`
+  width: 600px;
+  height: 30px;
+  font-size: 16px;
+`;
+
+const StyledPostContentsTextA = styled.textarea`
+  width: 600px;
+  height: 300px;
+  margin-top: 50px;
+  font-size: 16px;
+`;
+
+const PostSubmutButton = styled.button`
+  margin-top: 15px;
+  padding: 15px 25px;
+  border-radius: 15px;
+  color: AAC4FF;
+  font-size: 16px;
+`;
+
+function WriteForm() {
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+  const dateObject = new Date();
+  const year = dateObject.getFullYear();
+  const month = dateObject.getUTCMonth() + 1;
+  const day = dateObject.getDate();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    let object = {
+      title: title,
+      date: `${year}년 ${month}월 ${day}일`,
+      contents: contents,
+    };
+    setpostItems((oldArray) => [object, ...oldArray]);
+    setIsWrite(false);
+  };
+
+  return (
+    <WirteContainer>
+      <WriteTitle>입력폼</WriteTitle>
+      <FormContainer>
+        <StyledPostTitleInput
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          placeholder="제목을 입력해 주세용."
+        />
+        <StyledPostContentsTextA
+          onChange={(e) => {
+            setContents(e.target.value);
+          }}
+          placeholder="내용을 입력해 주세용."
+        />
+        <PostSubmutButton onClick={onSubmit}>작성완료</PostSubmutButton>
+      </FormContainer>
+    </WirteContainer>
+  );
+}
+
+export default WriteForm;
